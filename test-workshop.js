b9a5b9ab47162bcd1bb0333e14bdd5ea2a3fa39f
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-22 12:15:32 -0500

    functional tests

--- test-workshop.js
+++ test-workshop.js
@@ -26,12 +26,21 @@ Unit Tests:
 /*
 Prompt1
 Functional Tests:
-- When user does ____, _____ should happen
-- 
-
-Prompt2
-Functional Tests:
-- When user does ____, _____ should happen
-
-
+- When user does ____, _____ will happen
+- When user adds something to their cart, it will show up in their cart
+- User can add as many items in their cart
+- When user clicks to view their cart, it will bring them to their cart
+- When user is viewing their cart, the cost of each item will appear
+- When user is viewing their cart, the sum of the costs of all their items will appear at the bottom in bold
+- If cart is empty and the user tries to check out, a message will appear saying "Cart is Empty"
+- When user is not logged into an account and hits the "Checkout" button, then the page will be redirected to a prompt asking user to sign-in, create an account, or checkout as guest
+- If user decides to sign in after hitting checkout button, their cart will be saved.
+- After user signs in during checkout process, it will bring the user to the next step which is filling in address, card information, and billing address. 
+- If user signs in, then their address, card info, and billing info will be auto-filled in.
+- If user does not have an account and wants to create an account then they will be re-directed to a page to create an account.
+- If user does not sign in and continues as guest user will be re-directed to page for Shipping Address, Card Information, and Billing Address. 
+- When user creates an account, they will be asked to verify their email address before continuing. 
+- Account will not be created until email is verified
+- Once account is created user will be re-directed to page for Shipping Address, Card Information, and Billing Address. The page will look the same as Guest with no information pre-filled out.
+- If user creates an account, the information inputted will be saved and stored for future purchases.
 */
