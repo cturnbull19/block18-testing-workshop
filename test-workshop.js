44b9cccd414156904526416bdd3139f38893c52a
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-22 11:42:20 -0500

    finisihed Unit Testing Prompts

---
+++ test-workshop.js
@@ -0,0 +1,37 @@
+/*
+Prompt1
+Unit Tests:
+- Expect ____ to be a _____
+- Expect this function to multiply two numbers together
+- Expect the output to be a sinlge number
+- Expect to return the product of the two inputted numbers
+- If only one argument is given, then an error will occur
+- Expect multiplication(2, 3) to be 6
+- Expect multiplication("a", 3) to be an error
+- Expect muliplicaiton (2, 3, 8) to be an error
+- If no arguments are given then error
+- If one argument is given then error
+
+Prompt2
+Unit Tests:
+- Expect ____ to be a _____
+- Expect concatOdds to take two arrays and return a single array only including the odd numbers in ascending order
+- Expect concatOdds([3,2,1], [9,7,3,5,2,4,-1]) to be [-1, 1, 3, 5, 7, 9]
+- If there are multiple odd numbers, the output will include only one of them
+- If no odd numbers are within the array then output will be false
+- If there are negative numbers, then output will record the odd negative numbers
+- If there is a string included in the array then it will result in an error
+*/
+
+/*
+Prompt1
+Functional Tests:
+- When user does ____, _____ should happen
+- 
+
+Prompt2
+Functional Tests:
+- When user does ____, _____ should happen
+
+
+*/
